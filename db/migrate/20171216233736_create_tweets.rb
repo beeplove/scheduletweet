9e60f68239818e24194f4691c9b47751b0dacf1f
Mohammad Khan <mohammad.khan@gmx.us>
2017-12-16 16:57:50 -0800

    user and tweet association

--- db/migrate/20171216233736_create_tweets.rb
+++ db/migrate/20171216233736_create_tweets.rb
@@ -1,7 +1,7 @@
 class CreateTweets < ActiveRecord::Migration[5.1]
   def change
     create_table :tweets do |t|
-      t.references :user
+      t.belongs_to :user, index: true
       t.string :tweet, limit: 280
       t.datetime :scheduled_at
       t.timestamps
