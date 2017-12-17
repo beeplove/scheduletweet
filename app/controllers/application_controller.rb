c54d4a2401b469c0345f9aad06d6267da9590d99
Mohammad Khan <mohammad.khan@gmx.us>
2017-12-16 20:14:54 -0800

    added authenticated? helper method and minor refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
+  def authenticated?
+    !!current_user
+  end
+  private :authenticated?
+  helper_method :authenticated?
+
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
