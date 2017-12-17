8f3893b753c7e604b9093a81c7ddabf2e9f24e8d
Mohammad Khan <mohammad.khan@gmx.us>
2017-12-16 21:36:12 -0800

    new tweet to create tweet for user, still very basic

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,6 +6,24 @@ class TweetsController < ApplicationController
   end
 
   def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(permitted_params)
+    @tweet.user = current_user
+
+    if @tweet.save
+      redirect_to user_tweets_path(current_user)
+    else
+      render "new"
+    end
 
   end
+
+  def permitted_params
+    params.require(:tweet).permit(:tweet, :scheduled_at)
+  end
+  private :permitted_params
+
 end
