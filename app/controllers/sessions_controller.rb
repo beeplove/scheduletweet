4cc93deecc1db6823a6d4be9615071aa165eced9
Mohammad Khan <mohammad.khan@gmx.us>
2017-12-16 16:13:58 -0800

    added barebone tweets controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.from_omniauth(request.env['omniauth.auth'])
-    redirect_to "/"
+    redirect_to user_tweets_path(user)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [] do
+    resources :tweets
+  end
+
   match 'auth/twitter/callback', to: 'sessions#create', via: [:get]
 
 end
