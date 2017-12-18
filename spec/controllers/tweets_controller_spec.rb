07c6784395fa935cdf90022f94901ec4c2711bfe
Mohammad Khan <mohammad.khan@gmx.us>
2017-12-18 15:56:24 -0800

    added test cases for TweetsController#new

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -3,31 +3,62 @@ require 'rails_helper'
 RSpec.describe TweetsController, type: :controller do
   describe "GET index" do
     before(:each) do
-      @tweet = FactoryBot.create(:tweet)
+      @user = FactoryBot.create(:user)
     end
 
-    context "user is not logged in" do
+    context "when user is not logged in" do
       before(:each) do
         session[:current_user] = nil
       end
 
-      it "redirec to '/'" do
+      it "redirect to '/'" do
         get :index, params: { user_id: 1 }
         expect(response).to redirect_to root_path
       end
     end
 
-    context "user is logged in" do
+    context "when user is logged in" do
       before(:each) do
-        session[:current_user] = @tweet.user_id
-        allow(request.env['test']).to receive(:authenticate)
-        allow(controller).to receive(:current_user).and_return(@tweet.user)
+        session[:current_user] = @user.id
+        allow(controller).to receive(:authenticate)
+        allow(controller).to receive(:current_user).and_return(@user)
       end
 
-      it "redirec to '/'" do
-        get :index, params: { user_id: 1 }
+      it "renders 'index' template" do
+        get :index, params: { user_id: @user.id }
         expect(response).to render_template("index")
       end
     end
   end
+
+  describe "GET new" do
+    before(:each) do
+      @user = FactoryBot.create(:user)
+    end
+
+    context "when user is not logged in" do
+      before(:each) do
+        session[:current_user] = nil
+      end
+
+      it "redirect to '/'" do
+        get :new, params: { user_id: @user.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "when user is logged in" do
+      before(:each) do
+        session[:current_user] = @user.id
+        allow(controller).to receive(:authenticate)
+        allow(controller).to receive(:current_user).and_return(@user)
+      end
+
+      it "renders 'new' template" do
+        get :new, params: { user_id: @user.id }
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
 end
