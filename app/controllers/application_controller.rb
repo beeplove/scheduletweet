2f38e998533c5340f81a570da5eda976332e195e
Mohammad Khan <mohammad.khan@gmx.us>
2017-12-18 16:40:07 -0800

    cover the scenario when database and session is out of sync

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,10 +16,12 @@ class ApplicationController < ActionController::Base
   private :authenticated?
   helper_method :authenticated?
 
-
-  # TODO: need to cover the scenario: in case of corrupted session, it might be out of sync with database
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  rescue ActiveRecord::RecordNotFound
+    # in case of corrupted session, it might be out of sync with database
+    session[:user_id] = nil
+    @current_user = nil
   end
   private :current_user
   helper_method :current_user
