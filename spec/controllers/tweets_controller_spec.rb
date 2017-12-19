4e58280942f11af3c6c02d918978ff8f69f4f8ae
Mohammad Khan <mohammad.khan@gmx.us>
2017-12-18 16:28:13 -0800

    test coverage of happy path of create action

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe TweetsController, type: :controller do
         session[:current_user] = nil
       end
 
-      it "redirect to '/'" do
+      it "redirects to '/'" do
         get :index, params: { user_id: 1 }
         expect(response).to redirect_to root_path
       end
@@ -41,7 +41,7 @@ RSpec.describe TweetsController, type: :controller do
         session[:current_user] = nil
       end
 
-      it "redirect to '/'" do
+      it "redirects to '/'" do
         get :new, params: { user_id: @user.id }
         expect(response).to redirect_to root_path
       end
@@ -60,5 +60,39 @@ RSpec.describe TweetsController, type: :controller do
       end
     end
   end
+
+  describe "POST /" do
+    before(:each) do
+      @user = FactoryBot.create(:user)
+    end
+
+    context "when user is not logged in" do
+      before(:each) do
+        session[:current_user] = nil
+      end
+
+      it "redirects to '/'" do
+        get :new, params: { user_id: @user.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+
+    context "when user is logged in" do
+      before(:each) do
+        session[:current_user] = @user.id
+        allow(controller).to receive(:authenticate)
+        allow(controller).to receive(:current_user).and_return(@user)
+        allow(ScheduleTweetWorker).to receive(:perform_at)
+      end
+
+      it "redirects to '/users/:user_id/tweets'" do
+        date = 2.days.from_now.strftime("%m/%d/%Y")
+        post :create, params: { user_id: @user.id, tweet: {tweet: "Hello World!", scheduled_at: "#{date} 04:22 PM -0800"} }
+
+        expect(response).to redirect_to user_tweets_path(@user)
+      end
+    end
+  end
+
 
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,10 @@ FactoryBot.define do
 
   factory :user do
     name "John Smith"
+    provider "twitter"
+    uid "134922012"
+    token "RvMfR5usfNY9eNN1CzOwXY2C3o57VbmhEHiFmAwO"
+    secret "S8z0oRUh9ApBNJIcyyaWBXrfAFHGrbAUDw4LQlUNhxpoZ"
   end
 
   factory :tweet do
