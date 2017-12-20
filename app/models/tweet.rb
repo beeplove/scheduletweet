730bc87e900991b346976ed11121f6b46a7421c0
Mohammad Khan <mohammad.khan@gmx.us>
2017-12-20 11:44:00 -0800

    Tweet model validations

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,16 @@
 class Tweet < ApplicationRecord
   belongs_to :user
 
-  validates :tweet, presence: true
-  validates :tweet, length: { maximum: 280 }
-  validates :scheduled_at, presence: true
+  validates :tweet, presence: { message: "Tweet can't be empty" }
+  validates :tweet, length: { maximum: 280, message: "Tweet can't be more than 280 characters" }
+  validate  :scheduled_at, :validates_scheduled_at_in_future_time
+
+
+  def validates_scheduled_at_in_future_time
+    if scheduled_at.nil?
+      errors.add(:scheduled_at, "Scheduled time can't be empty")
+    elsif Time.now > scheduled_at
+      errors.add(:scheduled_at, "Scheduled at can't be in past")
+    end
+  end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -2,16 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
   let (:tweet) { FactoryBot.build(:tweet) }
+  let (:user) { tweet.user }
 
   describe "#save" do
     it "saves tweet in the database" do
-      user = tweet.user
       tweet = Tweet.new (attributes_for(:tweet))
       tweet.user = user
-      tweet.save!
+      tweet.save
       expect(tweet.new_record?).to be_falsey
     end
 
-    it "should require tweet "
+    it "should require tweet" do
+      tweet.tweet = nil
+      expect(tweet.valid?).to be_falsey
+    end
+
+    it "should require scheduled at time" do
+      tweet.scheduled_at = nil
+      expect(tweet.valid?).to be_falsey
+    end
+
+    it "raises error when scheduled at time is in past" do
+      tweet.scheduled_at = 1.day.ago
+      expect(tweet.valid?).to be_falsey
+    end
   end
 end
